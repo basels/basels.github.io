4b58505ab8a6329f46ff7908b6da7c9b3bc09f27
Basel Shbita <basel921@gmail.com>
2022-04-23 13:21:08 -0700

    update news & add more art & captions

--- js/app.js
+++ js/app.js
@@ -19,13 +19,28 @@ class galImgEle {
 }
 
 const imageNamesAnimation = [
-    new galImgEle('ramadan_cali_2022.gif', 'Ramadan 2022', 'JANUARY 4, 2022', 'Ramadan is community. Ramadan is reflection. Ramadan is fasting. Ramadan is prayer. Ramadan Kareem.<br><br>Short animated pixelart loop. Digital pen drawn in Adobe Photoshop then tweened in Adobe Animate.'),
-    new galImgEle('2022_tiger.gif', '2022. Year of the Tiger', 'JANUARY 4, 2022', 'A year summoning traits of bravery and competitiveness, but also generosity and charm.<br><br>Short animated loop. Digital pen drawn frame-by-frame in Adobe Animate.'),
-    new galImgEle('cali2pali.gif', 'From Cali to Pali', 'MAY 21, 2021', 'A call to Save Sheikh Jarrah and stop the attack on Gaza during the 2021 Israel–Palestine crisis.<br><br>Short animated loop. Digital pen drawn frame-by-frame in Adobe Animate.'),
-    new galImgEle('womensday.gif', "Women's Day 2021", 'MARCH 8, 2021', "Celebrating all women around the globe on International Women's Day.<br><br>Short animated loop. Digital pen drawn frame-by-frame in Adobe Animate with pop-up elements combined."),
-    new galImgEle('astronaut.gif', 'No land to fall into', 'MARCH 30, 2021', 'Endless fall, into infinity and beyond, into self and outwards.<br><br>Short animated loop. Digital pen drawn frame-by-frame in Adobe Animate.'),
+    new galImgEle('ramadan_cali_2022.gif', 'Ramadan 2022', 'JANUARY 4, 2022', 'Ramadan is community. Ramadan is reflection. Ramadan is fasting. Ramadan is prayer. Ramadan Kareem.<br><br>Short animated pixelart loop. Drawn with drawing-pad in Adobe Photoshop then tweened in Adobe Animate.'),
+    new galImgEle('2022_tiger.gif', '2022. Year of the Tiger', 'JANUARY 4, 2022', 'A year summoning traits of bravery and competitiveness, but also generosity and charm.<br><br>Short animated loop. Drawn with drawing-pad frame-by-frame in Adobe Animate.'),
+    new galImgEle('cali2pali.gif', 'From Cali to Pali', 'MAY 21, 2021', 'A call to Save Sheikh Jarrah and stop the attack on Gaza during the 2021 Israel–Palestine crisis.<br><br>Short animated loop. Drawn with drawing-pad frame-by-frame in Adobe Animate.'),
+    new galImgEle('womensday.gif', "Women's Day 2021", 'MARCH 8, 2021', "Celebrating all women around the globe on International Women's Day.<br><br>Short animated loop. Drawn with drawing-pad frame-by-frame in Adobe Animate with pop-up elements combined."),
+    new galImgEle('astronaut.gif', 'No land to fall into', 'MARCH 30, 2021', 'Endless fall, into infinity and beyond, into self and outwards.<br><br>Short animated loop. Drawn with drawing-pad frame-by-frame in Adobe Animate.'),
     new galImgEle('falling.gif', 'Are you woke?', 'JUNE 6, 2020', "If you let go, you shall find, your true self.<br><br>Short animated loop. Hand-drawn then post-processed in Adobe Photoshop."),
-    new galImgEle('balloon.gif', 'Untitled (Draft#008)', 'MARCH 12, 2020', "In my world of my own, this balloon can be anything<br><br>Short animated loop. Hand-drawn.")];
+    new galImgEle('balloon.gif', 'Untitled (Draft#008)', 'MARCH 12, 2020', "In my world of my own, this balloon can be anything.<br><br>Short animated loop. Hand-drawn.")];
+
+const imageNamesPainting = [
+    new galImgEle('pnt_09.jpg', 'Portrait of Lina Shbita', 'MARCH 15, 2022', 'Acrylic & collage (mixed media) on canvas.'),
+    new galImgEle('pnt_08.jpg', 'Portrait of Hadar Cohen', 'MARCH 2, 2022', 'Acrylic & collage (mixed media) on canvas.'),
+    new galImgEle('pnt_07.jpg', 'Portrait of Nader Sid-Ahmad & Sara Osman', 'FEBRUARY 27, 2022', 'Acrylic & collage (mixed media) on canvas.'),
+    new galImgEle('pnt_06.jpg', 'Portrait of Brandy Carstens &  Athier Mousawi', 'JUNE 6, 2021', 'Acrylic on canvas.'),
+    new galImgEle('pnt_05.jpg', 'Portrait of Hala Kaddoura', 'APRIL 19, 2020', 'Acrylic & collage (mixed media) on canvas.'),
+    new galImgEle('pnt_04.jpg', 'Suba', 'DECEMBER 29, 2006', 'Taken from a larger pirate-themed family portrait.<br><br>Acrylic on canvas.'),
+    new galImgEle('pnt_03.jpg', 'Haram esh-Sharif, Jerusalem', 'SEPTEMBER 8, 2006', 'Dome of the Rock & the Haram esh-Sharif compound in East Jerusalem.<br><br>Acrylic on canvas.')];
+
+const imageNamesGraphicDesign = [
+    new galImgEle('purple_man_001.jpg', 'PurpleMan#01', 'NOVEMBER 8, 2021', 'The first digital collage in the twisted multi-dimensionally interlaced "Purple Man" series.<br><br>Digital pop art collage made with Adobe Photoshop.'),
+    new galImgEle('spacething.jpg', 'Andromeda, we have a problem.', 'MARCH 6, 2021', "The mind is a tool, you can hear it out, but you don't have to follow its orders. Ask it to move over. Take over the driver's seat. You are in charge now.<br><br>Digital pop art collage made with Adobe Photoshop."),
+    new galImgEle('ancients.jpg', 'I am mine and (belong) only to myself', 'JUNE 28, 2020', 'The psychological chains and patriarchal warfare are soon to be broken by the chain-breaker.<br><br>Digital pop art collage made with Adobe Photoshop.')];
+
 
 const imageNamesCreativeCoding = [
 new galImgEle('escaping_the_covid.gif', 'Escaping the COVID', 'APRIL 5, 2020', 'Escaping to a different realm.<br><br>20-second video made from an art-coding project during the COVID-19 pandemic. Generated using processing IDE (some graphics was drawn on Adobe Photoshop, some adopted from Nintendo (thanks!)).'),
@@ -35,11 +50,6 @@ new galImgEle('escaping_the_covid.gif', 'Escaping the COVID', 'APRIL 5, 2020', '
     new galImgEle('bezier_03.gif', 'Chann3l#03', 'MARCH 10, 2021', 'Semi-random Bézier curves generation #03.<br><br>Short animated loop. Generated using processing IDE.'),
     new galImgEle('bezier_04.gif', 'Chann3l#04', 'MARCH 10, 2021', 'Semi-random Bézier curves generation #04.<br><br>Short animated loop. Generated using processing IDE.')];
 
-const imageNamesGraphicDesign = [
-    new galImgEle('purple_man_001.jpg', 'PurpleMan#01', 'NOVEMBER 8, 2021', 'The first digital collage in the twisted multi-dimensionally interlaced "Purple Man" series.<br><br>Digital pop art collage made with Adobe Photoshop.'),
-    new galImgEle('spacething.jpg', 'Andromeda, we have a problem.', 'MARCH 6, 2021', "The mind is a tool, you can hear it out, but you don't have to follow its orders. Ask it to move over. Take over the driver's seat. You are in charge now.<br><br>Digital pop art collage made with Adobe Photoshop."),
-    new galImgEle('ancients.jpg', 'I am mine and (belong) only to myself', 'JUNE 28, 2020', 'The psychological chains and patriarchal warfare are soon to be broken by the chain-breaker.<br><br>Digital pop art collage made with Adobe Photoshop.')];
-
 const imageNamesDrawing = ['dig_art_01.jpg',
     'sketch_01.jpg', 'sketch_02.jpg', 'sketch_03.jpg', 'sketch_04.jpg',
     'fd_07_01.jpg', 'fd_07_02.jpg', 'fd_07_03.jpg', 'fd_07_04.jpg',
@@ -47,8 +57,6 @@ const imageNamesDrawing = ['dig_art_01.jpg',
     'fd_05_01.jpg', 'fd_05_02.jpg',
     'fd_04_01.jpg', 'fd_04_02.jpg',
     'fd_03_01.jpg', 'fd_03_02.jpg', 'fd_03_03.jpg', 'fd_03_04.jpg'];
-
-const imageNamesPainting = ['pnt_05.jpg', 'pnt_04.jpg', 'pnt_03.jpg']
 
 const imageNamesPlasticArts = ['clay.jpg', 'concrete.jpg', 'gypsum.jpg', 'plasticine.jpg']
 
@@ -69,19 +77,19 @@ imageNamesAnimation.forEach(i => {
     gallery_animation.appendChild(image);
 })
 
-imageNamesCreativeCoding.forEach(i => {
+imageNamesPainting.forEach(i => {
     const image = document.createElement('img');
     image.src = 'images/art/thumbs/thumb_' + i.filename;
     image.classList.add('galleryImg');
 
     image.addEventListener('click', () => {
         popup.style.transform = 'translateY(0)';
-        selectedImage.src = 'images/art/gifs/' + i.filename;
+        selectedImage.src = 'images/art/painting/' + i.filename;
         selectedImageHeader.innerHTML = i.title;
         selectedImageSubHeader.innerHTML = i.subtitle;
         selectedImageBody.innerHTML = i.body;
     })
-    gallery_creativecoding.appendChild(image);
+    gallery_painting.appendChild(image);
 })
 
 imageNamesGraphicDesign.forEach(i => {
@@ -99,6 +107,21 @@ imageNamesGraphicDesign.forEach(i => {
     gallery_graphicdesign.appendChild(image);
 })
 
+imageNamesCreativeCoding.forEach(i => {
+    const image = document.createElement('img');
+    image.src = 'images/art/thumbs/thumb_' + i.filename;
+    image.classList.add('galleryImg');
+
+    image.addEventListener('click', () => {
+        popup.style.transform = 'translateY(0)';
+        selectedImage.src = 'images/art/gifs/' + i.filename;
+        selectedImageHeader.innerHTML = i.title;
+        selectedImageSubHeader.innerHTML = i.subtitle;
+        selectedImageBody.innerHTML = i.body;
+    })
+    gallery_creativecoding.appendChild(image);
+})
+
 imageNamesDrawing.forEach(i => {
     const image = document.createElement('img');
     image.src = 'images/art/thumbs/thumb_' + i;
@@ -110,19 +133,6 @@ imageNamesDrawing.forEach(i => {
         selectedImageHeader.innerHTML = '<i>Untitled</i>';
     })
     gallery_drawing.appendChild(image);
-})
-
-imageNamesPainting.forEach(i => {
-    const image = document.createElement('img');
-    image.src = 'images/art/thumbs/thumb_' + i;
-    image.classList.add('galleryImg');
-
-    image.addEventListener('click', () => {
-        popup.style.transform = 'translateY(0)';
-        selectedImage.src = 'images/art/painting/' + i;
-        selectedImageHeader.innerHTML = '<i>Untitled</i>';
-    })
-    gallery_painting.appendChild(image);
 })
 
 imageNamesPlasticArts.forEach(i => {
